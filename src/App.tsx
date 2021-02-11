71c07c29324cb0d34ca38c9be61804c05e2e817f
xiavn <lianne.turnbull@gmail.com>
2021-02-11 21:10:58 +0000

    Set up fetching of info

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,36 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import fetchGraphQL from "./fetchGraphQL";
 
 function App() {
+    const [name, setName] = useState(null);
+    useEffect(() => {
+        let isMounted = true;
+        fetchGraphQL(`
+		query RespositoryNameQuery {
+			respository(owner: "xiavn" name: "relay-test") {
+				name
+			}
+		}
+      `)
+            .then((response) => {
+                if (!isMounted) {
+                    return;
+                }
+                const data = response.data;
+                setName(data.repository.name);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+        return () => {
+            isMounted = false;
+        };
+    }, [fetchGraphQL]);
     return (
         <div className="App">
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
+                <p>{name != null ? `Repository: ${name}` : "Loading"}</p>
             </header>
         </div>
     );
